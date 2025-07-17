8d64f41bf6d0124c5aaf64fc223492ffc7709c76
Lars <pinguinlars1105@gmail.com>
2025-07-17 21:08:30 +0200

    Starship config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,17 @@
 if status is-interactive
-  set -x EDITOR nvim
+    set -x EDITOR nvim
 end
 
 function fish_user_key_bindings
-  fish_vi_key_bindings --no-erase insert
+    fish_vi_key_bindings --no-erase insert
 end
 
 function fish_greeting
 
 end
+
+function starship_transient_prompt_func
+    starship module character
+end
+starship init fish | source
+#enable_transience
